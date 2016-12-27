f845c4e85b325d563d26210cc5575b867cb418d8
houdunwang <2300071698@qq.com>
2016-12-27 13:17:30 +0800

    no message

---
+++ src/Rbac.php
@@ -0,0 +1,45 @@
+<?php
+/** .-------------------------------------------------------------------
+ * |  Software: [HDCMS framework]
+ * |      Site: www.hdcms.com
+ * |-------------------------------------------------------------------
+ * |    Author: 向军 <2300071698@qq.com>
+ * |    WeChat: aihoudun
+ * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
+ * '-------------------------------------------------------------------*/
+namespace houdunwang\rbac;
+use houdunwang\rbac\build\Base;
+
+class Rbac {
+	//连接
+	protected $link;
+
+	//更改缓存驱动
+	protected function driver() {
+		$this->link = new Base( $this );
+
+		return $this;
+	}
+
+	public function __call( $method, $params ) {
+		if ( is_null( $this->link ) ) {
+			$this->driver();
+		}
+
+		return call_user_func_array( [ $this->link, $method ], $params );
+	}
+
+	public static function __callStatic( $name, $arguments ) {
+		static $link;
+		if ( is_null( $link ) ) {
+			$link = new static();
+		}
+
+		return call_user_func_array( [ $link, $name ], $arguments );
+	}
+}
+
+
+
+
+
